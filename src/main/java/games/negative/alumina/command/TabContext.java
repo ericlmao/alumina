4a066b9ce7c30136e469431b27804fd176d65751
Eric <negggative@gmail.com>
2023-09-17 15:25:29 -0300

    Expand on `TabContext`

--- src/main/java/games/negative/alumina/command/TabContext.java
+++ src/main/java/games/negative/alumina/command/TabContext.java
@@ -1,4 +1,58 @@
+/*
+ *  MIT License
+ *
+ * Copyright (C) 2023 Negative Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package games.negative.alumina.command;
 
-public record TabContext() {
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+/**
+ * Represents the context of a tab completion.
+ * @param sender The sender of the command.
+ * @param args The arguments of the command.
+ */
+public record TabContext(@NotNull CommandSender sender, @NotNull String[] args) {
+
+    /**
+     * Gets the argument at the specified index.
+     * @param index The index of the argument.
+     * @return The argument at the specified index.
+     */
+    @NotNull
+    public Optional<String> argument(int index) {
+        return index < args.length ? Optional.of(args[index]) : Optional.empty();
+    }
+
+    /**
+     * Gets the last argument.
+     * @return The last argument.
+     */
+    @NotNull
+    public String lastArgument() {
+        return argument(args.length - 1).orElseThrow(() -> new IllegalStateException("No arguments were provided."));
+    }
 }
