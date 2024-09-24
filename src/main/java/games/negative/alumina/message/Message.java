0acc31f436e9d1934d52e3d8e6663453acc5ef20
Eric <negggative@gmail.com>
2024-09-24 16:57:41 -0300

    Add custom tags to `Message` and more

--- src/main/java/games/negative/alumina/message/Message.java
+++ src/main/java/games/negative/alumina/message/Message.java
@@ -28,17 +28,21 @@
 package games.negative.alumina.message;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import games.negative.alumina.logger.Logs;
 import games.negative.alumina.util.MiniMessageUtil;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.Tag;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a message that can be sent to a {@link Audience}.
@@ -50,6 +54,10 @@ import java.util.List;
 public class Message {
 
     private static Component PREFIX;
+
+    private static final Map<String, Tag> CUSTOM_TAGS = Maps.newConcurrentMap();
+
+    private static MiniMessage miniMessage;
 
     /*
      * This is the default, unmodified message.
@@ -74,7 +82,6 @@ public class Message {
         this.papi = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
     }
 
-
     /**
      * Send the final message to a {@link Audience}.
      *
@@ -88,13 +95,28 @@ public class Message {
     }
 
     /**
-     * Send the final message to an iterable collection of a class that extends {@link Audience}
+     * Send the final message to a {@link Audience}.
      *
-     * @param iterable The iterable collection of a class that extends {@link Audience}
-     * @param <T>      The class that extends {@link Audience}
+     * @param audience The recipient of the message.
      */
-    public <T extends Iterable<? extends Audience>> void send(@NotNull final T iterable) {
-        send(iterable, (String) null);
+    @SafeVarargs
+    public final void send(@NotNull Audience audience, @Nullable Map.Entry<String, Component>... placeholders) {
+        Preconditions.checkNotNull(audience, "Audience cannot be null.");
+
+        Component component = asComponent(audience, placeholders);
+        audience.sendMessage(component);
+    }
+
+    /**
+     * Send the final message to a {@link Audience}.
+     *
+     * @param audience The recipient of the message.
+     */
+    public void send(@NotNull Audience audience) {
+        Preconditions.checkNotNull(audience, "Audience cannot be null.");
+
+        Component component = asComponent(audience);
+        audience.sendMessage(component);
     }
 
     /**
@@ -109,6 +131,35 @@ public class Message {
 
         for (Audience audience : iterable) {
             send(audience, placeholders);
+        }
+    }
+
+    /**
+     * Send the final message to an iterable collection of a class that extends {@link Audience}
+     * @param iterable The iterable collection of a class that extends {@link Audience}
+     * @param placeholders The optional key-value pairs of placeholders to replace in the message.
+     * @param <T> The class that extends {@link Audience}
+     */
+    public <T extends Iterable<? extends Audience>> void send(@NotNull T iterable, @Nullable Map.Entry<String, Component>... placeholders) {
+        Preconditions.checkNotNull(iterable, "Iterable cannot be null.");
+        Preconditions.checkArgument(iterable.iterator().hasNext(), "Iterable cannot be empty.");
+
+        for (Audience audience : iterable) {
+            send(audience, placeholders);
+        }
+    }
+
+    /**
+     * Send the final message to an iterable collection of a class that extends {@link Audience}
+     * @param iterable The iterable collection of a class that extends {@link Audience}
+     * @param <T> The class that extends {@link Audience}
+     */
+    public <T extends Iterable<? extends Audience>> void send(@NotNull T iterable) {
+        Preconditions.checkNotNull(iterable, "Iterable cannot be null.");
+        Preconditions.checkArgument(iterable.iterator().hasNext(), "Iterable cannot be empty.");
+
+        for (Audience audience : iterable) {
+            send(audience);
         }
     }
 
@@ -128,6 +179,23 @@ public class Message {
     public void broadcast(@Nullable Audience audience, @Nullable String... placeholders) {
         Component component = asComponent(audience, placeholders);
         Bukkit.getServer().broadcast(component);
+    }
+
+    /**
+     * Broadcast the final message to the server.
+     */
+    public void broadcast() {
+        broadcast(null, (String[]) null);
+    }
+
+    /**
+     * Converts the message to a Component.
+     * @param audience The audience to display the message to.
+     * @return The Component representation of the message.
+     */
+    @NotNull
+    public Component asComponent(@Nullable Audience audience) {
+        return asComponent(audience, (String[]) null);
     }
 
     /**
@@ -154,7 +222,7 @@ public class Message {
                 String replacement = placeholders[i + 1];
 
                 if (placeholder == null || replacement == null) {
-                    Logs.WARNING.print("Placeholder of " + placeholder + " has result of " + replacement + ". None of these value can be null. Skipping.", true);
+                    Logs.warning("Placeholder of " + placeholder + " has result of " + replacement + ". None of these value can be null. Skipping.", true);
                     continue;
                 }
 
@@ -162,9 +230,66 @@ public class Message {
             }
         }
 
-        Component component = MiniMessageUtil.translate(current);
+        if (miniMessage == null) {
+            miniMessage = MiniMessage.builder().editTags(builder -> {
+                for (Map.Entry<String, Tag> entry : CUSTOM_TAGS.entrySet()) {
+                    builder.tag(entry.getKey(), entry.getValue());
+                }
+            }).build();
+        }
+
+        Component component = MiniMessageUtil.translate(current, miniMessage);
         if (PREFIX != null) {
             component = component.replaceText(TextReplacementConfig.builder().matchLiteral("%prefix%").replacement(PREFIX).build());
+        }
+
+        return component;
+    }
+
+    /**
+     * Converts the message to a Component.
+     *
+     * @param audience     The audience to display the message to.
+     * @param placeholders The optional key-value pairs of placeholders to replace in the message.
+     * @return The Component representation of the message.
+     * @throws NullPointerException     if the audience is null.
+     * @throws IllegalArgumentException if the number of placeholders is not even.
+     */
+    @SafeVarargs
+    @NotNull
+    public final Component asComponent(@Nullable Audience audience, @Nullable Map.Entry<String, Component>... placeholders) {
+        String current = content;
+        if (papi) {
+            current = me.clip.placeholderapi.PlaceholderAPI.setPlaceholders((audience instanceof Player player ? player : null), current);
+        }
+
+        if (miniMessage == null) {
+            miniMessage = MiniMessage.builder().editTags(builder -> {
+                for (Map.Entry<String, Tag> entry : CUSTOM_TAGS.entrySet()) {
+                    builder.tag(entry.getKey(), entry.getValue());
+                }
+            }).build();
+        }
+
+        Component component = MiniMessageUtil.translate(current, miniMessage);
+        if (PREFIX != null) {
+            component = component.replaceText(TextReplacementConfig.builder().matchLiteral("%prefix%").replacement(PREFIX).build());
+        }
+
+        if (placeholders != null) {
+            for (Map.Entry<String, Component> placeholder : placeholders) {
+                if (placeholder == null) continue;
+
+                String key = placeholder.getKey();
+                Component value = placeholder.getValue();
+
+                if (key == null || value == null) {
+                    Logs.warning("Placeholder of " + key + " has result of " + value + ". None of these value can be null. Skipping.", true);
+                    continue;
+                }
+
+                component = component.replaceText(TextReplacementConfig.builder().matchLiteral(key).replacement(value).build());
+            }
         }
 
         return component;
@@ -219,6 +344,15 @@ public class Message {
     }
 
     /**
+     * Register a custom minimessage tag for global use in all messages.
+     * @param name The name of the tag.
+     * @param tag The tag to register.
+     */
+    public static void registerTag(@NotNull String name, @NotNull Tag tag) {
+        CUSTOM_TAGS.put(name, tag);
+    }
+
+    /**
      * Set the prefix of the message.
      * @param component The prefix of the message.
      */
@@ -229,5 +363,10 @@ public class Message {
     @Nullable
     public static Component prefix() {
         return PREFIX;
+    }
+
+    @NotNull
+    public static Map<String, Tag> customTags() {
+        return CUSTOM_TAGS;
     }
 }
